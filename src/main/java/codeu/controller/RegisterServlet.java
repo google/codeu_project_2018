3a5ba578fec1d325c35c12ce8489ece38c1eda21
Kevin Workman <kevinaworkman@gmail.com>
2018-04-29 17:27:36 -0700

    Adding prototype for registration page.

---
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -0,0 +1,17 @@
+package codeu.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegisterServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+    request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
+  }
+}
