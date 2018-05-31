3b61f90fa87023caebe22784ee2dd9008390a41e
Dan Garrette <dhgarrette@gmail.com>
2018-05-31 16:34:32 -0400

    Add comments

--- src/test/java/codeu/model/data/ModelDataTestHelpers.java
+++ src/test/java/codeu/model/data/ModelDataTestHelpers.java
@@ -1,3 +1,17 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package codeu.model.data;
 
 import java.time.Instant;
@@ -7,6 +21,7 @@ import org.junit.Assert;
 
 public class ModelDataTestHelpers {
 
+  /** Asserts that all fields on both Messages are the same. */
   public static void assertMessageEquals(Message expectedMessage, Message actualMessage) {
     Assert.assertEquals(expectedMessage.getId(), actualMessage.getId());
     Assert.assertEquals(expectedMessage.getConversationId(), actualMessage.getConversationId());
@@ -15,6 +30,14 @@ public class ModelDataTestHelpers {
     Assert.assertEquals(expectedMessage.getCreationTime(), actualMessage.getCreationTime());
   }
 
+  /**
+   * Use this to create a fake Message to use in a unit test. When created it contains random data
+   * in every field, and the individual methods can be used to set the test conditions. For example,
+   * if the test needs specific author ID and message, then you could do:
+   *
+   * <p>UUID fakeAuthor = UUID.randomUUID(); String fakeMessage = "test message 1"; Message
+   * fakeMessage = new TestMessageBuilder().author(fakeAuthor).message(fakeMessage).build();
+   */
   public static class TestMessageBuilder {
     public TestMessageBuilder() {
       this.id = UUID.randomUUID();
