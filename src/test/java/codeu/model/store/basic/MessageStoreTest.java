18694b919c958c83be17be7fcb421d8b40cdedb0
Dan Garrette <dhgarrette@gmail.com>
2018-06-01 09:52:30 -0400

    Undoing changes to MessageStoreTest since they are in another PR

--- src/test/java/codeu/model/store/basic/MessageStoreTest.java
+++ src/test/java/codeu/model/store/basic/MessageStoreTest.java
@@ -1,21 +1,16 @@
 package codeu.model.store.basic;
 
-import static codeu.model.data.ModelDataTestHelpers.assertMessageEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
+import codeu.model.data.Message;
+import codeu.model.store.persistence.PersistentStorageAgent;
+import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import codeu.model.data.Message;
-import codeu.model.data.ModelDataTestHelpers.TestMessageBuilder;
-import codeu.model.store.persistence.PersistentStorageAgent;
 
 public class MessageStoreTest {
 
@@ -23,53 +18,72 @@ public class MessageStoreTest {
   private PersistentStorageAgent mockPersistentStorageAgent;
 
   private final UUID CONVERSATION_ID_ONE = UUID.randomUUID();
+  private final Message MESSAGE_ONE =
+      new Message(
+          UUID.randomUUID(),
+          CONVERSATION_ID_ONE,
+          UUID.randomUUID(),
+          "message one",
+          Instant.ofEpochMilli(1000));
+  private final Message MESSAGE_TWO =
+      new Message(
+          UUID.randomUUID(),
+          CONVERSATION_ID_ONE,
+          UUID.randomUUID(),
+          "message two",
+          Instant.ofEpochMilli(2000));
+  private final Message MESSAGE_THREE =
+      new Message(
+          UUID.randomUUID(),
+          UUID.randomUUID(),
+          UUID.randomUUID(),
+          "message three",
+          Instant.ofEpochMilli(3000));
 
   @Before
   public void setup() {
     mockPersistentStorageAgent = Mockito.mock(PersistentStorageAgent.class);
     messageStore = MessageStore.getTestInstance(mockPersistentStorageAgent);
+
+    final List<Message> messageList = new ArrayList<>();
+    messageList.add(MESSAGE_ONE);
+    messageList.add(MESSAGE_TWO);
+    messageList.add(MESSAGE_THREE);
+    messageStore.setMessages(messageList);
   }
 
   @Test
   public void testGetMessagesInConversation() {
-    final Message message1 = new TestMessageBuilder().conversation(CONVERSATION_ID_ONE).build();
-    final Message message2 = new TestMessageBuilder().build();
-    final Message message3 = new TestMessageBuilder().conversation(CONVERSATION_ID_ONE).build();
-    messageStore.setMessages(Arrays.asList(message1, message2, message3));
-
     List<Message> resultMessages = messageStore.getMessagesInConversation(CONVERSATION_ID_ONE);
 
-    assertEquals(2, resultMessages.size());
-    assertMessageEquals(message1, resultMessages.get(0));
-    assertMessageEquals(message3, resultMessages.get(1));
-  }
-
-  @Test
-  public void testGetMessagesInConversation_noMessagesFound() {
-    final Message message1 = new TestMessageBuilder().build();
-    final Message message2 = new TestMessageBuilder().build();
-    final Message message3 = new TestMessageBuilder().build();
-    messageStore.setMessages(Arrays.asList(message1, message2, message3));
-
-    UUID unusedId = UUID.randomUUID();
-    List<Message> resultMessages = messageStore.getMessagesInConversation(unusedId);
-
-    assertTrue(resultMessages.isEmpty());
+    Assert.assertEquals(2, resultMessages.size());
+    assertEquals(MESSAGE_ONE, resultMessages.get(0));
+    assertEquals(MESSAGE_TWO, resultMessages.get(1));
   }
 
   @Test
   public void testAddMessage() {
-    final List<Message> messageList = new ArrayList<>();
-    messageList.add(new TestMessageBuilder().build());
-    messageList.add(new TestMessageBuilder().build());
-    messageStore.setMessages(messageList);
+    UUID inputConversationId = UUID.randomUUID();
+    Message inputMessage =
+        new Message(
+            UUID.randomUUID(),
+            inputConversationId,
+            UUID.randomUUID(),
+            "test message",
+            Instant.now());
 
-    final Message message3 = new TestMessageBuilder().conversation(CONVERSATION_ID_ONE).build();
-    messageStore.addMessage(message3);
+    messageStore.addMessage(inputMessage);
+    Message resultMessage = messageStore.getMessagesInConversation(inputConversationId).get(0);
 
-    List<Message> resultMessages = messageStore.getMessagesInConversation(CONVERSATION_ID_ONE);
-    assertEquals(1, resultMessages.size());
-    assertMessageEquals(message3, resultMessages.get(0));
-    Mockito.verify(mockPersistentStorageAgent).writeThrough(message3);
+    assertEquals(inputMessage, resultMessage);
+    Mockito.verify(mockPersistentStorageAgent).writeThrough(inputMessage);
+  }
+
+  private void assertEquals(Message expectedMessage, Message actualMessage) {
+    Assert.assertEquals(expectedMessage.getId(), actualMessage.getId());
+    Assert.assertEquals(expectedMessage.getConversationId(), actualMessage.getConversationId());
+    Assert.assertEquals(expectedMessage.getAuthorId(), actualMessage.getAuthorId());
+    Assert.assertEquals(expectedMessage.getContent(), actualMessage.getContent());
+    Assert.assertEquals(expectedMessage.getCreationTime(), actualMessage.getCreationTime());
   }
 }
