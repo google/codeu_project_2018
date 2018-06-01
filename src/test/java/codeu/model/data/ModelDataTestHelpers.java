ef90fd3a8e9475b80b20c8ff4b6dee57dfbf595f
Dan Garrette <dhgarrette@gmail.com>
2018-06-01 01:15:15 -0400

    Improvements to unit test of Store classes

--- src/test/java/codeu/model/data/ModelDataTestHelpers.java
+++ src/test/java/codeu/model/data/ModelDataTestHelpers.java
@@ -17,17 +17,82 @@ package codeu.model.data;
 import java.time.Instant;
 import java.util.UUID;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 
 public class ModelDataTestHelpers {
 
+  /** Asserts that all fields on both Conversations are the same. */
+  public static void assertConversationEquals(Conversation expected, Conversation actual) {
+    assertEquals(expected.getId(), actual.getId());
+    assertEquals(expected.getOwnerId(), actual.getOwnerId());
+    assertEquals(expected.getTitle(), actual.getTitle());
+    assertEquals(expected.getCreationTime(), actual.getCreationTime());
+  }
+
   /** Asserts that all fields on both Messages are the same. */
-  public static void assertMessageEquals(Message expectedMessage, Message actualMessage) {
-    Assert.assertEquals(expectedMessage.getId(), actualMessage.getId());
-    Assert.assertEquals(expectedMessage.getConversationId(), actualMessage.getConversationId());
-    Assert.assertEquals(expectedMessage.getAuthorId(), actualMessage.getAuthorId());
-    Assert.assertEquals(expectedMessage.getContent(), actualMessage.getContent());
-    Assert.assertEquals(expectedMessage.getCreationTime(), actualMessage.getCreationTime());
+  public static void assertMessageEquals(Message expected, Message actual) {
+    assertEquals(expected.getId(), actual.getId());
+    assertEquals(expected.getConversationId(), actual.getConversationId());
+    assertEquals(expected.getAuthorId(), actual.getAuthorId());
+    assertEquals(expected.getContent(), actual.getContent());
+    assertEquals(expected.getCreationTime(), actual.getCreationTime());
+  }
+
+  /** Asserts that all fields on both Messages are the same. */
+  public static void assertUserEquals(User expected, User actual) {
+    assertEquals(expected.getId(), actual.getId());
+    assertEquals(expected.getName(), actual.getName());
+    assertEquals(expected.getPasswordHash(), actual.getPasswordHash());
+    assertEquals(expected.getCreationTime(), actual.getCreationTime());
+  }
+
+  /**
+   * Use this to create a fake Conversation to use in a unit test. When created it contains random
+   * data in every field, and the individual methods can be used to set the test conditions. For
+   * example, if the test needs specific owner ID and title, then you could do:
+   *
+   * <pre>{@code
+   * UUID fakeOwner = UUID.randomUUID();
+   * String fakeTitle = "test title 1";
+   * Conversation fakeConversation = new TestConversationBuilder().owner(fakeOwner).title(fakeTitle).build();
+   * }</pre>
+   */
+  public static class TestConversationBuilder {
+    public TestConversationBuilder() {
+      this.id = UUID.randomUUID();
+      this.owner = UUID.randomUUID();
+      this.title = UUID.randomUUID().toString();
+      this.creation = Instant.now();
+    }
+
+    public TestConversationBuilder id(UUID id) {
+      this.id = id;
+      return this;
+    }
+
+    public TestConversationBuilder owner(UUID owner) {
+      this.owner = owner;
+      return this;
+    }
+
+    public TestConversationBuilder title(String title) {
+      this.title = title;
+      return this;
+    }
+
+    public TestConversationBuilder creation(Instant creation) {
+      this.creation = creation;
+      return this;
+    }
+
+    public Conversation build() {
+      return new Conversation(id, owner, title, creation);
+    }
+
+    private UUID id;
+    private UUID owner;
+    private String title;
+    private Instant creation;
   }
 
   /**
@@ -83,6 +148,54 @@ public class ModelDataTestHelpers {
     private UUID conversation;
     private UUID author;
     private String content;
+    private Instant creation;
+  }
+
+  /**
+   * Use this to create a fake User to use in a unit test. When created it contains random data in
+   * every field, and the individual methods can be used to set the test conditions. For example, if
+   * the test needs specific name, then you could do:
+   *
+   * <pre>{@code
+   * String fakeName = "Alex Smith";
+   * User fakeUser = new TestUserBuilder().name(fakeName).build();
+   * }</pre>
+   */
+  public static class TestUserBuilder {
+    public TestUserBuilder() {
+      this.id = UUID.randomUUID();
+      this.name = UUID.randomUUID().toString();
+      this.passwordHash = UUID.randomUUID().toString();
+      this.creation = Instant.now();
+    }
+
+    public TestUserBuilder id(UUID id) {
+      this.id = id;
+      return this;
+    }
+
+    public TestUserBuilder name(String name) {
+      this.name = name;
+      return this;
+    }
+
+    public TestUserBuilder passwordHash(String passwordHash) {
+      this.passwordHash = passwordHash;
+      return this;
+    }
+
+    public TestUserBuilder creation(Instant creation) {
+      this.creation = creation;
+      return this;
+    }
+
+    public User build() {
+      return new User(id, name, passwordHash, creation);
+    }
+
+    private UUID id;
+    private String name;
+    private String passwordHash;
     private Instant creation;
   }
 }
