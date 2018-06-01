0322b72c50fb5c5653b3ee085d532327b853469f
Dan Garrette <dhgarrette@gmail.com>
2018-06-01 18:53:15 -0400

    Use "diamond operator" for generic collections.

--- src/test/java/codeu/model/store/basic/MessageStoreTest.java
+++ src/test/java/codeu/model/store/basic/MessageStoreTest.java
@@ -42,7 +42,7 @@ public class MessageStoreTest {
     List<Message> resultMessages = messageStore.getMessagesInConversation(CONVERSATION_ID_ONE);
 
     assertEquals(2, resultMessages.size());
-    Map<UUID, Message> resultMessagesSet = new HashMap<UUID, Message>();
+    Map<UUID, Message> resultMessagesSet = new HashMap<>();
     for (Message resultMessage : resultMessages) {
       resultMessagesSet.put(resultMessage.getId(), resultMessage);
     }
@@ -75,7 +75,7 @@ public class MessageStoreTest {
 
     List<Message> resultMessages = messageStore.getMessagesInConversation(CONVERSATION_ID_ONE);
     assertEquals(1, resultMessages.size());
-    Map<UUID, Message> resultMessagesSet = new HashMap<UUID, Message>();
+    Map<UUID, Message> resultMessagesSet = new HashMap<>();
     for (Message resultMessage : resultMessages) {
       resultMessagesSet.put(resultMessage.getId(), resultMessage);
     }
